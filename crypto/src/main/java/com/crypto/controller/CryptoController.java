758e36a2a3ec77e18988ed82b530cb2345301200
Jr-Jader <juuniorcaardoso@gmail.com>
2024-02-21 10:11:31 -0300

    delet crypto

--- crypto/src/main/java/com/crypto/controller/CryptoController.java
+++
@@ -1,8 +0,0 @@
-package com.crypto.controller; // OBS: Controllers establish communication with views and the other parts
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class CryptoController {
-
-}
--- crypto/src/main/java/com/crypto/repository/MercadoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crypto.repository; // OBS: Repositories have direct access to the DB
-
-import com.crypto.models.Mercado;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MercadoRepository extends CrudRepository<Mercado, String> {
-}
